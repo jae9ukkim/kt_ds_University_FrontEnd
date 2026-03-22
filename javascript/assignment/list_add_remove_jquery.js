c6b0dcec39d35e4c04d035a0e91285a912c8ea92
jae9ukkim <jaegogjaegog@gmail.com>
2026-03-22 22:05:57 +0900

    assign

--- javascript/assignment/list_add_remove_jquery.js
+++ javascript/assignment/list_add_remove_jquery.js
@@ -1,1 +1,41 @@
-$().ready(function () {});
+$().ready(function () {
+  var listCnt = $("#btn-add").closest("div").find("ul").data("listcnt");
+
+  $("#btn-add").on("click", function () {
+    if (listCnt === 10) {
+      alert("더 이상 추가할 수 없습니다");
+      return;
+    }
+
+    var listItem = $("<li>");
+    listItem.text("아이템 목록 " + ++listCnt);
+    listItem.addClass("list-item");
+
+    $(this).closest("div").find("ul").append(listItem);
+    $(this)
+      .closest("div")
+      .find("div")
+      .text("총 " + listCnt + "개의 아이템이 등록되었습니다.");
+  });
+
+  $("#btn-remove").on("click", function () {
+    if (listCnt === 0) {
+      alert("이미 모든 아이템이 제거되었습니다");
+      return;
+    }
+
+    listCnt = 0;
+
+    $(this)
+      .closest("div")
+      .find("ul")
+      .children("li")
+      .each(function () {
+        $(this).remove();
+      });
+    $(this)
+      .closest("div")
+      .find("div")
+      .text("총 " + listCnt + "개의 아이템이 등록되었습니다.");
+  });
+});
--- javascript/assignment/list_add_remove_vanilla.js
+++ javascript/assignment/list_add_remove_vanilla.js
@@ -1,1 +1,38 @@
-window.onload = function () {};
+window.onload = function () {
+  var listCnt =
+    document.querySelector("#btn-add").parentElement.children[2].dataset
+      .listcnt;
+
+  document.querySelector("#btn-add").addEventListener("click", function () {
+    if (listCnt === 10) {
+      alert("더 이상 추가할 수 없습니다");
+      return;
+    }
+
+    var listItem = document.createElement("li");
+    listItem.className = "list-item";
+    listItem.innerText = "아이템 목록 " + ++listCnt;
+
+    this.parentElement.children[2].appendChild(listItem);
+
+    this.parentElement.children[3].innerText =
+      "총 " + listCnt + "개의 아이템이 등록되었습니다.";
+  });
+
+  document.querySelector("#btn-remove").addEventListener("click", function () {
+    if (listCnt === 0) {
+      alert("이미 모든 아이템이 제거되었습니다");
+      return;
+    }
+
+    var listArray = this.parentElement.children[2].children;
+
+    while (listArray.length > 0) {
+      this.parentElement.children[2].removeChild(listArray[0]);
+    }
+
+    listCnt = 0;
+    this.parentElement.children[3].innerText =
+      "총 " + listCnt + "개의 아이템이 등록되었습니다.";
+  });
+};
